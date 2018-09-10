c54154ad65fbfa7b997934499c97dc2383f5aca5
shantanu <shantanu.gade@cuelogic>
2018-09-10 13:42:31 +0530

    added comment component and added CURD operation on comments

---
+++ Components/Comments/Models/commentModel.js
@@ -0,0 +1,68 @@
+import { connection } from '../../../app';
+
+
+class commentModel {
+
+    postComment = (req, res, next) => {
+        let queryString = 'INSERT INTO comments(postId,userId,commentContent)VALUES(?,?,?)';
+        let values = [req.body.postId, req.body.userId, req.body.commentContent];
+
+        connection.query(queryString, values, (err, result, feilds) => {
+            if (err) {
+                res.json({ success: false, message: err });
+            }
+        
+            else {
+                res.json(result);
+            }
+        });
+    }
+
+    getAllComments = (req, res, next) => {
+        let queryString = 'SELECT * FROM comments WHERE postId = ?';
+        let values = [req.params.postId];
+
+        connection.query(queryString, values, (err, result, fields) => {
+            if (err) {
+                res.json({ success: false, message: err });
+            }
+            else if (result.length === 0) {
+                res.json({success:false, message:"No comments available on this post"})
+            }
+            else {
+                res.json({ result });
+            }
+        });
+    }
+
+    deleteComment = (req,res,next) => {
+        let queryString = 'DELETE FROM comments WHERE commentId = ?';
+        let values = [req.body.commentId];
+
+        connection.query(queryString,values,(err,result,fields)=> {
+            if(err) {
+                res.json({success:false, message: err});
+            }
+            else {
+                res.json(result);
+            }
+        })
+    }
+
+    updateComment = (req,res,next) => {
+        let queryString = 'UPDATE comments SET commentContent = ? WHERE commentId = ?';
+        let values = [req.body.commentContent , req.body.commentId];
+
+        connection.query(queryString,values,(err, result, fields) => {
+            if(err) {
+                res.json({success:false, message : err});
+            }
+            else {
+                res.json(result)
+            }
+        });
+    }
+
+}
+
+export default new commentModel();
